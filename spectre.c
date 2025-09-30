5088f3faf55d43c6fc23c6bd6764b76fb5f188ea
Biro Shin <biroshin@caen-vnc-mi01.engin.umich.edu>
2025-09-30 19:09:37 -0400

    timing code

--- spectre.c
+++ spectre.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <sys/time.h>
+#include <math.h>
+#include <x86intrin.h>
 
 int main(int argc, char **argv) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    uint64_t start = tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+
     printf("hello world!\n");
 
-    unsigned long t1, t2;
-
-
+    gettimeofday(&tv, NULL);
+    uint64_t end = tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+    
+    uint64_t elapsed = end - start;
+    printf("time elapsed : %lld\n", elapsed);
     return 0;
 }
